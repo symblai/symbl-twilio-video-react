3fc110d071aaa21965e5eef29e3cb547a9d0a127
unknown <Amanpreetsinghbedi_19012722.cse@geu.ac.in>
2021-10-19 15:29:41 +0530

    screen share participant message added

--- src/components/MainParticipant/MainParticipant.js
+++ src/components/MainParticipant/MainParticipant.js
@@ -5,11 +5,17 @@ import useMainSpeaker from '../../hooks/useMainSpeaker/useMainSpeaker';
 import useSelectedParticipant from '../VideoProvider/useSelectedParticipant/useSelectedParticipant';
 import useScreenShareParticipant from '../../hooks/useScreenShareParticipant/useScreenShareParticipant';
 import Transcript from "../Transcript/Transcript";
+import useVideoContext from "../../hooks/useVideoContext/useVideoContext";
 
 export default function MainParticipant() {
+
   const mainParticipant = useMainSpeaker();
   const [selectedParticipant] = useSelectedParticipant();
   const screenShareParticipant = useScreenShareParticipant();
+  
+  const {
+    room: { localParticipant },
+  } = useVideoContext();
 
   const videoPriority =
     mainParticipant === selectedParticipant || mainParticipant === screenShareParticipant ? 'high' : null;
@@ -17,9 +23,13 @@ export default function MainParticipant() {
   return (
     /* audio is disabled for this participant component because this participant's audio 
        is already being rendered in the <ParticipantStrip /> component.  */
-    <MainParticipantInfo participant={mainParticipant}>
+    <MainParticipantInfo participant={mainParticipant} screenShareParticipant={screenShareParticipant} localParticipant={localParticipant}>
       <ParticipantTracks participant={mainParticipant} disableAudio enableScreenShare videoPriority={videoPriority} mainPariticipant={true}/>
       <Transcript height={"100%"}/>
     </MainParticipantInfo>
   );
 }
+
+
+
+
