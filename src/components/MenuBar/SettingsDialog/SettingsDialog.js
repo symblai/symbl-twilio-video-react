af2f46984774c675efb6d9aa88168ae6cec0d725
Vanshaj Poonia <74147463+VanshajPoonia@users.noreply.github.com>
2021-10-30 22:27:18 +0530

    Update SettingsDialog.js

--- src/components/MenuBar/SettingsDialog/SettingsDialog.js
+++ src/components/MenuBar/SettingsDialog/SettingsDialog.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Dialog, DialogActions, Tab, Tabs, Theme} from '@material-ui/core';
+import {Button, Dialog, DialogActions, Tab, Tabs} from '@material-ui/core';
 import {createStyles, makeStyles} from '@material-ui/core/styles';
 import ConnectionOptions from '../ConnectionOptions/ConnectionOptions';
 import {DeviceSelector} from '../DeviceSelector/DeviceSelector';
